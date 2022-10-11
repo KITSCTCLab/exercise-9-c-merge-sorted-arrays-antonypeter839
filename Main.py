7c4f6052762cf2ef91b2b05430970d4e2f8ecf1d
antonypeter839 <110010533+antonypeter839@users.noreply.github.com>
2022-10-11 11:54:58 +0530

    t

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   nums1 = merge_sort(nums1)
   return nums1
   
-def merge_sort(myList) -> None:
+def merge_sort(myList) -> None
   if len(myList) > 1:
         mid = len(myList) // 2
         left = myList[:mid]
